898dbd34ef5b08268c056bfce17bf7537f39f5a6
allendavis-official <allendavis_official@hotmail.com>
2026-01-23 09:53:15 +0000

    Debuging

--- src/server.js
+++ src/server.js
@@ -25,9 +25,9 @@ const attendanceRoutes = require("./routes/attendanceRoutes");
 // Initialize Express app
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("SchoolLink API is running");
-});
+// app.get("/", (req, res) => {
+//   res.status(200).send("SchoolLink API is running");
+// });
 
 // Security middleware
 app.use(helmet());
